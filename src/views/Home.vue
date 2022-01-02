3e51d2d9560276c92def52a3a93f664652a9c6fd
Will Phillips <willadelinephillips@gmail.com>
2022-01-01 20:46:39 -0800

    Added personality

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Strazo logo" src="../assets/Strazo.png">
+    <HelloWorld msg="Welcome!"/>
   </div>
 </template>
 
