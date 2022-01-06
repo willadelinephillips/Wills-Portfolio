4fb2bbb618046b6e1caf613a9004896d8338c3c6
Will Phillips <willadelinephillips@gmail.com>
2022-01-05 19:25:48 -0800

    Updated main page

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,23 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      やれやれだぜ (Good Grief)!
-    </p>
+    <h3>やれやれだぜ (Good Grief)!</h3>
+    <h4>You seem to have found my portfolio</h4>
+    <p></p>
+    <h3>Looking for something in particular?</h3>
+    <ul>
+      <li><b>Skills:</b>
+          <ul>
+            <li>Tireless Worker</li>
+            <li>Motivated</li>
+          </ul>
+      </li>
+      <li><b>Languages:</b></li>
+      <ul>
+        <li>HTML/CSS</li>
+        <li>Javascript</li>
+      </ul>
+    </ul>
   </div>
 </template>
 
@@ -20,15 +34,7 @@ export default {
 <style scoped>
 h3 {
   margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+} 
 a {
   color: #42b983;
 }
