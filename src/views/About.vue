63c3e8033bf1d6189f1756c469a775da69ff7c23
Will Phillips <willadelinephillips@gmail.com>
2022-01-05 18:05:27 -0800

    Fleshed out About Me

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,5 +2,10 @@
   <div class="about">
     <img alt="Selfie logo" src="../assets/avatar.png">
     <h1>This is an about page</h1>
+    <h3>Y'know. About me and stuff.</h3>
+    <p>I do origami, write horror stories, and play an unhealthy amount of video games.</p>
+    <h3>Programming?</h3>
+    <p>I realized my passion for programming the second first toyed with HTML. All I want to do 
+      for the forseeable future is write code and create new things on the Web.</p>
   </div>
 </template>
