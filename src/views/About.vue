c1cc7db6baaf761ab33c46fde68838aadeda9509
Will Phillips <willadelinephillips@gmail.com>
2022-01-05 21:38:06 -0800

    I symmerty

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,7 +3,7 @@
     <img alt="Selfie logo" src="../assets/avatar.png" class="mainimg">
     <h1>This is an about page</h1>
     <h3>Y'know. About me and stuff.</h3>
-    <p>I do origami, write horror stories, and play an unhealthy amount of video games.</p>
+    <p>I do origami, I write horror stories, and I play an unhealthy amount of video games.</p>
     <h3>Programming?</h3>
     <p>I realized my passion for programming the second I first toyed with HTML. All I want to do 
       for the forseeable future is write code and create new things on the Web.</p>
