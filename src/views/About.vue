78d7d2f2c4e3355dcaeff328ae356ea6df3e0506
Will Phillips <willadelinephillips@gmail.com>
2022-01-05 21:42:00 -0800

    The Web

--- src/views/About.vue
+++ src/views/About.vue
@@ -6,7 +6,7 @@
     <p>I do origami, I write horror stories, and I play an unhealthy amount of video games.</p>
     <h3>Programming?</h3>
     <p>I realized my passion for programming the second I first toyed with HTML. All I want to do 
-      for the forseeable future is write code and create new things on the Web.</p>
+      for the forseeable future is write code and create new things on The Web.</p>
     <h3>Okay, but what about those rare occasions when you do leave the house?</h3>
     <p>I live in SoCal, where I go to Balboa Park and the beach. Again, only if forced to leave 
       the house.</p>
