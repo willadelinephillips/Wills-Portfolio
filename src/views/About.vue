f59c311861b5a69977f158d6059ecbb3b4388019
Will Phillips <willadelinephillips@gmail.com>
2022-01-05 21:28:47 -0800

    More spelling

--- src/views/About.vue
+++ src/views/About.vue
@@ -5,7 +5,7 @@
     <h3>Y'know. About me and stuff.</h3>
     <p>I do origami, write horror stories, and play an unhealthy amount of video games.</p>
     <h3>Programming?</h3>
-    <p>I realized my passion for programming the second first toyed with HTML. All I want to do 
+    <p>I realized my passion for programming the second I first toyed with HTML. All I want to do 
       for the forseeable future is write code and create new things on the Web.</p>
     <h3>Okay, but what about those rare occasions that you do leave the house?</h3>
     <p>I live in SoCal, where I go to Balboa Park and the beach. Again, only if forced to leave 
