8aa119db830798c8c0b0b6d852158914c866c55b
Will Phillips <willadelinephillips@gmail.com>
2022-01-05 17:45:01 -0800

    Added typer

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,5 +2,8 @@
   <div class="about">
     <img alt="Selfie logo" src="../assets/avatar.png">
     <h1>This is an about page</h1>
+    <h3>Y'know. About me and stuff.</h3>
+    <p>I make origami. I play a whole lot of video games. I write short horror stories.</p>
+    <h3>What do you mean missing end tag?</h3>
   </div>
 </template>
