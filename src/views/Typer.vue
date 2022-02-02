fe589135961f8732785e779c9ca9ab0b050d70f9
Will Phillips <willadelinephillips@gmail.com>
2022-02-02 09:27:26 -0800

    Revert "Fixed typer perhaps"
    This reverts commit 08091f9ae51f3f7260a22f200fb61987599a2ba6.

--- src/views/Typer.vue
+++ src/views/Typer.vue
@@ -2,6 +2,8 @@
   <div class="container">
     <h1>
       <span class="typed-text">{{ typeValue }}</span>
+      <br>
+      <span class="typed-text">Yeet?</span>
     </h1>
   </div>
 </template>
@@ -13,7 +15,7 @@ import { setTimeout } from 'timers';
       return {
         typeValue: '',
         typeStatus: false,
-        typeArray: ['Hello there', 'I see you have found the sample of my Typer'],
+        typeArray: ['Hello there', 'I see you have found the About section.'],
         typingSpeed: 100,
         erasingSpeed: 10,
         newTextDelay: 2000,
