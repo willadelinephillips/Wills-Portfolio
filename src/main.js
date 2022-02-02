9be8dd26bd2814811c70fae650dceae829dc366e
Will Phillips <willadelinephillips@gmail.com>
2022-02-02 09:28:25 -0800

    Revert "Added working typer"
    This reverts commit 131ec7c4cc0c00163afb850f86aa4afe7e63f426.

--- src/main.js
+++ src/main.js
@@ -3,13 +3,11 @@ import App from './App.vue'
 import { createRouter, createWebHistory } from "vue-router";
 import About from "./views/About.vue";
 import Home from "./views/Home.vue";
-import Typer from "./views/Typer.vue";
 
 
 const routes = [
     { path: "/", component: Home },
     { path: "/about", component: About },
-    { path: "/typer", component: Typer},
   ];
   
   const router = createRouter({
